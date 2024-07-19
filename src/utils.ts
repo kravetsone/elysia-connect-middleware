e346e7531ace9a63a849d028d19632f1cc9c2f1e
Kravets <57632712+kravetsone@users.noreply.github.com>
2024-07-20 00:43:07 +0300

    chore: add query params parser

--- src/utils.ts
+++ src/utils.ts
@@ -1,42 +1,36 @@
-import { IncomingMessage, type ServerResponse } from "node:http";
-import { Socket } from "node:net";
-import { Readable } from "node:stream";
+import type { ServerResponse } from "node:http";
 import {
 	type MockResponse,
 	type RequestOptions,
 	createRequest,
 } from "node-mocks-http";
 
-// const incomingMessage = new IncomingMessage(new Socket());
-// incomingMessage.method = request.method;
-// incomingMessage.headers = request.headers.toJSON();
-// incomingMessage.url = request.url;
-// if (request.body) {
-// 	const readable = Readable.fromWeb(request.body);
-// 	incomingMessage.push(readable);
-// }
-// return incomingMessage;
-
 export function transformRequestToIncomingMessage(
 	request: Request,
 	options?: RequestOptions,
 ) {
 	const parsedURL = new URL(request.url, "http://localhost");
+
+	const query: Record<string, unknown> = {};
+	for (const [key, value] of parsedURL.searchParams.entries()) {
+		query[key] = value;
+	}
+
 	const message = createRequest({
 		method: request.method.toUpperCase() as "GET",
 		url: parsedURL.pathname,
 		headers: request.headers.toJSON(),
+		query,
+		// body: request.body,
 		...options,
 	});
-	console.log(message.url, parsedURL);
+	console.log(message.query, parsedURL);
 	return message;
 }
 
 export function transformResponseToServerResponse(
 	serverResponse: MockResponse<ServerResponse>,
 ) {
-	console.log(serverResponse, serverResponse._getData());
-
 	return new Response(serverResponse._getData(), {
 		status: serverResponse.statusCode,
 		statusText: serverResponse.statusMessage,
