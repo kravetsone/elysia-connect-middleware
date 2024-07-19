750794e0157c9b43632f3b18c7e8326459577af3
Kravets <57632712+kravetsone@users.noreply.github.com>
2024-07-20 00:46:15 +0300

    chore: add tests

---
+++ tests/index.test.ts
@@ -0,0 +1,59 @@
+import { describe, expect, it } from "bun:test";
+import { Elysia } from "elysia";
+import { connect } from "../src";
+
+describe("My own middleware", () => {
+	it("Simple create header middleware", async () => {
+		const HEADER_NAME = "elysia-middlewares";
+		const EXPECTED_VALUE = "connect";
+
+		const app = new Elysia().use(
+			connect((req, res, next) => {
+				res.setHeader(HEADER_NAME, EXPECTED_VALUE);
+				next();
+			}),
+		);
+
+		const response = await app.handle(new Request("http://localhost/"));
+
+		expect(response.status).toBe(404);
+		expect(response.headers.get(HEADER_NAME)).toBe(EXPECTED_VALUE);
+	});
+
+	it("Simple parse query-params", async () => {
+		const EXPECTED_VALUE = "KRAVETSONE";
+
+		const app = new Elysia().use(
+			connect((req, res, next) => {
+				res.end(req.query.value);
+			}),
+		);
+
+		const response = await app.handle(
+			new Request(`http://localhost/?value=${EXPECTED_VALUE}`),
+		);
+
+		expect(response.status).toBe(200);
+		expect(await response.text()).toBe(EXPECTED_VALUE);
+	});
+
+	// it("Simple parse body", async () => {
+	// 	const EXPECTED_VALUE = "KRAVETSONE";
+
+	// 	const app = new Elysia().use(
+	// 		connect((req, res, next) => {
+	// 			console.log(req.body);
+	// 			res.end(req.body);
+	// 		}),
+	// 	);
+
+	// 	const response = await app.handle(
+	// 		new Request("http://localhost/", {
+	// 			body: JSON.stringify({ value: "EXPECTED_VALUE" }),
+	// 		}),
+	// 	);
+
+	// 	expect(response.status).toBe(200);
+	// 	expect(await response.json()).toBe(EXPECTED_VALUE);
+	// });
+});
---
+++ tests/third-party.test.ts
@@ -0,0 +1,87 @@
+import { describe, expect, it } from "bun:test";
+import { Elysia } from "elysia";
+import { connect } from "../src";
+
+describe("Connect middleware", () => {
+	it("Use cors() middleware", async () => {
+		const app = new Elysia().use(
+			connect(
+				require("cors")({
+					origin: "http://example.com",
+				}),
+			),
+		);
+
+		const response = await app.handle(new Request("http://localhost/"));
+
+		expect(response.status).toBe(404);
+		expect(response.headers.get("access-control-allow-origin")).toBe(
+			"http://example.com",
+		);
+		expect(response.headers.get("vary")).toBe("Origin");
+	});
+	it("Use static() from express-static middleware", async () => {
+		const app = new Elysia().use(connect(require("express-static")(".")));
+
+		const response = await app.handle(
+			new Request("http://localhost/README.md"),
+		);
+
+		expect(response.status).toBe(200);
+		expect(response.headers.get("content-type")).toContain("markdown");
+	});
+	it("Use helmet() middleware", async () => {
+		const app = new Elysia().use(connect(require("helmet")()));
+
+		const response = await app.handle(new Request("http://localhost/"));
+		console.log(response);
+
+		expect(response.status).toBe(404);
+		expect(response.headers.get("content-security-policy")).toBeString();
+	});
+	it("Use rateLimit() from express-rate-limit middleware", async () => {
+		const app = new Elysia()
+			.use(
+				connect(
+					(await import("express-rate-limit")).rateLimit({
+						limit: 1,
+					}),
+				),
+			)
+			.get("/", "Hello, world!");
+
+		const response = await app.handle(new Request("http://localhost/"));
+
+		expect(response.status).toBe(200);
+		expect(response.headers.get("x-ratelimit-remaining")).toBe("0");
+
+		const response2 = await app.handle(new Request("http://localhost/"));
+
+		expect(response2.status).toBe(429);
+		expect(response2.headers.get("retry-after")).toBe("60");
+	});
+
+	// it("Use processImage() from express-processimage middleware", async () => {
+	// 	const ROOT = "./tests/assets";
+
+	// 	const app = new Elysia().use(
+	// 		connect(
+	// 			(await import("express-processimage")).default({
+	// 				root: ROOT,
+	// 			}),
+	// 			require("express-static")(ROOT),
+	// 		),
+	// 	);
+
+	// 	const response = await app.handle(
+	// 		new Request(
+	// 			"http://localhost/takodachi.png?resize=400,300&pngquant=128&pngcrush&setFormat=jpg",
+	// 		),
+	// 	);
+
+	// 	console.log(response);
+
+	// 	expect(response.status).toBe(200);
+	// 	// expect(response.headers.get("content-security-policy")).toBeString();
+	// });
+});
