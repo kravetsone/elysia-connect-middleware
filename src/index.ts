37f1ff526a00c91d68df06e1dbe2500030ef0f6c
Kravets <57632712+kravetsone@users.noreply.github.com>
2024-07-19 20:59:49 +0300

    feat: finally... middlewares in Elysia

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,59 @@
 import { IncomingMessage, ServerResponse } from "node:http";
 import { Socket } from "node:net";
+import Connect from "connect";
 import { Elysia } from "elysia";
+import { createResponse } from "node-mocks-http";
 import type { ConnectMiddleware } from "./types";
-import { transformRequestToIncomingMessage } from "./utils";
+import {
+	transformRequestToIncomingMessage,
+	transformResponseToServerResponse,
+} from "./utils";
+
+const connectApp = Connect();
 
 export function connect(...middlewares: ConnectMiddleware[]) {
+	for (const middleware of middlewares) {
+		connectApp.use(middleware);
+	}
+
+	// connectApp.use((err, req, res, next) => {
+	// 	console.log(err)
+	// })
+
 	return new Elysia({
 		name: "connect",
-	}).onRequest(async ({ request }) => {
+	}).onRequest(async ({ request, set }) => {
 		return await new Promise<Response | undefined>((resolve) => {
-			console.log("SO");
 			const message = transformRequestToIncomingMessage(request);
 
-			const response = new ServerResponse(new IncomingMessage(new Socket()));
+			const response = createResponse();
 
-			response.end = (data: any) =>
-				new Response(data, {
-					status: response.statusCode,
-					statusText: response.statusMessage,
-					// @ts-expect-error
-					headers: response.getHeaders(),
+			const end = response.end;
+
+			// @ts-expect-error
+			response.end = (...args: Parameters<typeof response.end>) => {
+				const call = end.call(response, ...args);
+
+				console.log(response);
+
+				const webResponse = transformResponseToServerResponse(response);
+
+				// if (response.writableEnded)
+				resolve(webResponse);
+
+				return call;
+			};
+
+			connectApp.handle(message, response, () => {
+				const webResponse = transformResponseToServerResponse(response);
+				webResponse.headers.forEach((value, key) => {
+					console.log(key, value);
+					set.headers[key] = value;
 				});
+				set.status = webResponse.status;
 
-			for (const middleware of middlewares) {
-				middleware(message, response, () => {
-					resolve(undefined);
-				});
-			}
-
-			return resolve(
-				new Response("SOO", {
-					status: response.statusCode,
-					statusText: response.statusMessage,
-					// @ts-expect-error
-					headers: response.getHeaders(),
-				}),
-			);
+				resolve(undefined);
+			});
 		});
 	});
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,28 +1,46 @@
-import { IncomingMessage, ServerResponse } from "node:http";
+import { IncomingMessage, type ServerResponse } from "node:http";
 import { Socket } from "node:net";
 import { Readable } from "node:stream";
+import {
+	type MockResponse,
+	type RequestOptions,
+	createRequest,
+} from "node-mocks-http";
 
-export function transformRequestToIncomingMessage(request: Request) {
-	const incomingMessage = new IncomingMessage(new Socket());
-	incomingMessage.method = request.method;
-	incomingMessage.headers = request.headers.toJSON();
-	incomingMessage.url = sanitizeUrl(request.url);
+// const incomingMessage = new IncomingMessage(new Socket());
+// incomingMessage.method = request.method;
+// incomingMessage.headers = request.headers.toJSON();
+// incomingMessage.url = request.url;
+// if (request.body) {
+// 	const readable = Readable.fromWeb(request.body);
+// 	incomingMessage.push(readable);
+// }
+// return incomingMessage;
 
-	if (request.body) {
-		const readable = Readable.fromWeb(request.body);
-		incomingMessage.push(readable);
-	}
-
-	return incomingMessage;
+export function transformRequestToIncomingMessage(
+	request: Request,
+	options?: RequestOptions,
+) {
+	const parsedURL = new URL(request.url, "http://localhost");
+	const message = createRequest({
+		method: request.method.toUpperCase() as "GET",
+		url: parsedURL.pathname,
+		headers: request.headers.toJSON(),
+		...options,
+	});
+	console.log(message.url, parsedURL);
+	return message;
 }
 
-function sanitizeUrl(url) {
-	/* eslint-disable-next-line no-var */
-	for (var i = 0, len = url.length; i < len; i++) {
-		const charCode = url.charCodeAt(i);
-		if (charCode === 63 || charCode === 35) {
-			return url.slice(0, i);
-		}
-	}
-	return url;
+export function transformResponseToServerResponse(
+	serverResponse: MockResponse<ServerResponse>,
+) {
+	console.log(serverResponse, serverResponse._getData());
+
+	return new Response(serverResponse._getData(), {
+		status: serverResponse.statusCode,
+		statusText: serverResponse.statusMessage,
+		// @ts-expect-error
+		headers: serverResponse.getHeaders(),
+	});
 }
