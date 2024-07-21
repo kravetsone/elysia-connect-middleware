7e67a7d8f90909c644a8e37d624e56a67e2f28c4
Kravets <57632712+kravetsone@users.noreply.github.com>
2024-07-21 13:13:59 +0300

    refactor: use NextFunction from express types

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,8 @@
-import type { Request, Response } from "express";
+import type { NextFunction, Request, Response } from "express";
 import type { MockRequest, MockResponse } from "node-mocks-http";
 
 export type ConnectMiddleware = (
 	req: MockRequest<Request>,
 	res: MockResponse<Response>,
-	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-	next: (err?: any) => unknown,
+	next: NextFunction,
 ) => unknown;
